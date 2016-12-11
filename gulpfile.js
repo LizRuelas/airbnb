ee3e1b549637181d2cd7161fa1ea7130f9e51f05
victor <victor.rojas@ec.pe>
2016-12-11 16:50:19 -0500

    subiendo gulp y express

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require("gulp"),
+        concat = require("gulp-concat"),
+        uglify = require("gulp-uglify"),
+        sass = require("gulp-sass"),
+        cleanCSS = require("gulp-clean-css");
+
+    gulp.task("devjs", function () {
+        gulp.src([
+            "node_modules/jquery/dist/*.min.js",
+            "node_modules/bootstrap/dist/js/*.min.js",
+            "src/js/*.js"])
+        .pipe(concat("main.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("dist/js/"))
+    });
+
+    gulp.task("devcss", function(){
+        gulp.src([
+            "node_modules/bootstrap/dist/css/*.min.css",
+            "src/sass/*.scss",
+            "src/css/*.css"
+            ])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat("main.css"))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("dist/css/"));
+    });
+
+    gulp.task('dev', ['devcss', 'devjs']);
