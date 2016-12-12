b0fe18b57e825501c3b1883d1d9fc5bbb52998ad
Sandy Ciquero <sandy.ciqueroch@gmail.com>
2016-12-12 10:27:18 -0500

    cambios en demo

--- dist/js/demo.js
+++ dist/js/demo.js
@@ -40,6 +40,45 @@ $(document).ready(function(){
         //     window.localStorage.setItem("destinatario", event.currentTarget.id);
         //      window.location.href = "/payment.html";
         // });
+
+
+        function initialize() {
+          var pyrmont = new google.maps.LatLng(-33.8665, 151.1956);
+
+          var map = new google.maps.Map(document.getElementById('map'), {
+            center: pyrmont,
+            zoom: 15,
+            scrollwheel: false
+          });
+
+          // Specify location, radius and place types for your Places API search.
+          var request = {
+            location: pyrmont,
+            radius: '500',
+            types: ['store']
+          };
+
+          // Create the PlaceService and send the request.
+          // Handle the callback with an anonymous function.
+          var service = new google.maps.places.PlacesService(map);
+          service.nearbySearch(request, function(results, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+              for (var i = 0; i < results.length; i++) {
+                var place = results[i];
+                // If the request succeeds, draw the place location on
+                // the map as a marker, and register an event to handle a
+                // click on the marker.
+            var marker = new google.maps.Marker({
+              map: map,
+              position: place.geometry.location
+            });
+      }
+    }
+  });
+}
+
+// Run the initialize function when the window has finished loading.
+google.maps.event.addDomListener(window, 'load', initialize);
  
     });
 });
