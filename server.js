d7ade4069b036182a00bff7dfb505e93d2423773
byOPKY <orrillokarol@gmail.com>
2016-12-12 18:29:30 -0500

    esqueleto

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.static(__dirname + '/dist')); // redirect root
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/assets', express.static(__dirname+'/assets'));
 //app.use(express.static(__dirname + "/public"));
 
 // ACAAAAAAAAAAAAAAAAAA
