f58a1c4af3bce6c6c8ea16e2a8b739e11157b17d
Alexandra Lopez <sccp.alopez@gmail.com>
2016-12-12 18:47:30 -0500

    estructura y estilos

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,39 @@
-$( document ).ready(function($) {
-  // $(".slider-testimonial").sss()//
+$( document ).ready(function() {
+  // experiencias//
+  (function(){
+  
+    var dly = 200;
+
+    $('.scroll-gal').each(function() { // caring about multi instances
+
+        var el = $(this),
+        ul = $('ul', el);
+
+        $('div', el).not('.vewport').click(function() {
+
+            // in case any animation in progress we do nothing
+            if( ul.is(':animated') ) return;
+
+            var first = $('li:first', ul),
+                last = $('li:last', ul);
+
+
+            switch( $(this).css('float') ) { // relaying on css floats which might be not the case for complex galleries
+                
+                case 'left':
+                    ul.css('left', -last.outerWidth(true)).prepend(last).animate({'left': 0}, dly);
+                break;
+                case 'right':
+                    ul.animate({'left': -first.outerWidth(true)}, dly, function(){
+                        $(this).append(first).css('left', 0)
+                    });
+                break;
+                
+            } // switch construction
+
+        }); // navigation click handler
+
+    }); // each loop
+
+})();
 });
