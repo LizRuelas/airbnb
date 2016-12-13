7f548dfb84310a62632f5ddd93df74a3e8c904fb
jenniferjara <lisbet.jara@gmail.com>
2016-12-13 12:37:45 -0500

    scss actualizado

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,28 @@
 var gulp = require("gulp"),
-        concat = require("gulp-concat"),
-        uglify = require("gulp-uglify"),
-        sass = require("gulp-sass"),
-        cleanCSS = require("gulp-clean-css");
+    concat = require("gulp-concat"),
+    uglify = require("gulp-uglify"),
+    sass = require("gulp-sass"),
+    cleanCSS = require("gulp-clean-css");
 
-    gulp.task("devjs", function () {
-        gulp.src([
-            "node_modules/jquery/dist/*.min.js",
-            "node_modules/bootstrap/dist/js/*.min.js",
-            "src/js/*.js"])
-        .pipe(concat("main.js"))
-        .pipe(uglify())
-        .pipe(gulp.dest("dist/js/"))
-    });
+gulp.task("devjs", function () {
+    gulp.src([
+        "node_modules/jquery/dist/*.min.js",
+        "node_modules/bootstrap/dist/js/*.min.js",
+        "src/js/*.js"])
+    .pipe(concat("main.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/js/"))
+});
 
-    gulp.task("devcss", function(){
-        gulp.src([
-            "node_modules/bootstrap/dist/css/*.min.css",
-            "src/css/*.min.css",
-            "src/sass/*.scss"])
-        .pipe(sass().on('error', sass.logError))
-        .pipe(concat("main.css"))
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("dist/css/"));
-    });
+gulp.task("devcss", function(){
+    gulp.src([
+        "node_modules/bootstrap/dist/css/*.min.css",
+        "src/css/*.min.css",
+        "src/sass/*.scss"])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat("main.css"))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest("dist/css/"));
+});
 
-    gulp.task('dev', ['devcss', 'devjs']);
+gulp.task('dev', ['devcss', 'devjs']);
