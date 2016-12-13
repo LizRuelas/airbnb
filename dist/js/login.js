75592b2fe3f6b6a6c2a7c6a957e72a4f80a68900
LizRuelas <liz.ruelas.borda@gmail.com>
2016-12-12 19:00:52 -0500

    sass login

--- dist/js/login.js
+++ dist/js/login.js
@@ -28,6 +28,7 @@ window.fbAsyncInit = function() {
 	    fbLogin.style.display = 'none';
 	    fbLogout.style.display = 'inline';
 	    status.style.display  = 'inline';
+	    status.style.float  = 'right';
 	    getUserInfo();
 	    
 	  } else {
@@ -63,3 +64,30 @@ window.fbAsyncInit = function() {
 	    });
 	  });
 	});
+	var googleUser = {};
+ 		var startApp = function() {
+    gapi.load('auth2', function(){
+      // Retrieve the singleton for the GoogleAuth library and set up the client.
+      auth2 = gapi.auth2.init({
+        client_id: '766741656408-rf4oag140490pdua7aanu3dvbm8ggfc6.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        // Request scopes in addition to 'profile' and 'email'
+        //scope: 'additional_scope'
+      });
+      attachSignin(document.getElementById('customBtn'));
+    });
+  };
+
+  function attachSignin(element) {
+    console.log(element.id);
+    auth2.attachClickHandler(element, {},
+        function(googleUser) {
+          document.getElementById('name').innerText = "Signed in: " +
+              googleUser.getBasicProfile().getName();
+        }, function(error) {
+          alert(JSON.stringify(error, undefined, 2));
+        });
+  }
+$(document).ready(function(){
+	startApp();
+});
