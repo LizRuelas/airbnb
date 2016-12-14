69d39d07360f6d9aeea9a800b8077074bd30e5a3
victor-pc <vico.16c@gmail.com>
2016-12-14 07:37:44 -0500

    agregando marcadores

--- dist/js/appLiz.js
+++ dist/js/appLiz.js
@@ -7,27 +7,15 @@ var cargarPagina = function() {
 var map;
 
 var json = [
-	{
-		"latitud" : -12.07411181223991,
-		"longitud" : -77.09629180717775,
-		"precio" : "$120"
-	},
+	["$120", -12.07411181223991, -77.09629180717775],
 
-	{
-		"latitud" : -12.079986982830766,
-		"longitud" : -77.09998252678224,
-		"precio" : "$120"
-	},
-
-	{
-		"latitud" : -12.069075849160106,
-		"longitud" : -77.10521819877931,
-		"precio" : "$120"
-	}
+	["$140",-12.079986982830766,-77.09998252678224],
+	
+	["$160",-12.069075849160106,-77.10521819877931]
 ]; 
 
-var funcionExito = function(posicion) {
-	var lat = posicion.coords.latitude;
+var funcionExito = function() {
+/*	var lat = posicion.coords.latitude;
     var lon = posicion.coords.longitude;
     var latlon = new google.maps.LatLng(lat, lon)
     var mapa = document.getElementById('mapa')
@@ -47,15 +35,39 @@ var funcionExito = function(posicion) {
     	position:latlon,
     	map:map,
     	title:"You are here!"
-    });
-
+    });*/
+    console.log(json);
+	console.log(json.length);
+	for (i=0 ; i<json.length ; i++){
+		
+		map = new google.maps.Map(document.getElementById('mapa'), {
+			zoom: 14,
+	        center: new google.maps.LatLng(-12.079986982830766, -77.09998252678224),
+	        mapTypeId: google.maps.MapTypeId.ROADMAP
+		});
+		 var infowindow = new google.maps.InfoWindow();
+		var marker, i;
+		for (i = 0; i < json.length; i++) {  
+		        marker = new google.maps.Marker({
+		          position: new google.maps.LatLng(json[i][1], json[i][2]),
+		          map: map
+		        });
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+          return function() {
+            infowindow.setContent(json[i][0]);
+            infowindow.open(map, marker);
+          }
+        })(marker, i));
+		 
+	}
     
  };
+};
 var funcionError = function (error) {
 	console.log(error);
 };
 
-var ubicar = function(){
+/*var ubicar = function(){
 	console.log(json);
 	console.log(json.length);
 	for (i=0 ; i<json.length ; i++){
@@ -74,12 +86,9 @@ var ubicar = function(){
 				    title: 'Hello World!'
 				  });
 			}
-}
+}*/
 	
-
-
 
 $(document).ready(function(){
 	cargarPagina();
-	ubicar();
 });
